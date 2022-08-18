6beb25624e8f4e7bda7da042066e6ffc8bad5c97
Andrey Baranov <baranov.a.64103@gmail.com>
2022-08-18 15:07:24 +0300

    nothing

---
+++ src/components/AdminLk.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminLk(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AdminLk;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,11 +13,13 @@ function Layout({ initState }) {
         <link rel="stylesheet" href="/stylesheets/normalize.css" />
         <link rel="stylesheet" href="/stylesheets/application.css" />
         <script dangerouslySetInnerHTML={{ __html: `window.initState=${JSON.stringify(initState)}` }} />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" crossOrigin="anonymous" />
         {/* Было раньше - <script defer>window.initState={{{initState}}}</script> */}
         <script defer src="/app.js" />
         <script defer src="/vendor.js" />
         <script defer src="https://api-maps.yandex.ru/2.1/?apikey=6eb8bf8f-1394-4692-b41f-0561df9b2c80&lang=ru_RU" type="text/javascript" />
-        <title>Document</title>
+        <title>Tea Blog</title>
       </head>
       <body>
         <div id="root">
