954c484e64d9beb7a590c43002b65067f68375a3
Denis Morozov <kadet14022002@mail.ru>
2022-08-19 10:23:57 +0300

    all the rest

--- src/components/AdminLk.jsx
+++ src/components/AdminLk.jsx
@@ -1,164 +1,159 @@
 import React, { useEffect, useState } from 'react';
 
-function AdminLk({userState}) {
-    const [allTea, setAllTea] = useState([{
-        name: '',
-        img: '',
-        description: '',
-        location: '',
-        x: 0.00,
-        y: 0.00,
-    }]);
-    const [inputTea, setInputTea] = useState([{
-            name: '',
-            img: '',
-            description: '',
-            location: '',
-            x: 0.00,
-            y: 0.00,
-        }]);
+function AdminLk({ userState }) {
+  const [allTea, setAllTea] = useState([{
+    name: '',
+    img: '',
+    description: '',
+    location: '',
+    x: 0.00,
+    y: 0.00,
+  }]);
+  const [inputTea, setInputTea] = useState([{
+    name: '',
+    img: '',
+    description: '',
+    location: '',
+    x: 0.00,
+    y: 0.00,
+  }]);
 
   useEffect(() => {
     fetch('/api/teas')
       .then((res) => res.json())
-      .then((data) => setAllTea(data))
+      .then((data) => setAllTea(data));
   }, []);
 
-console.log(allTea);
+  console.log(allTea);
 
+  const inputHandler = (e) => {
+    setInputTea((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/tea/add', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(inputTea),
+    });
+  };
+  return (
+    <>
+      <div>
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Имя</th>
+              <th scope="col">email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Данные:</th>
+              <td>{userState.name}</td>
+              <td>{userState.email}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-    
-      const inputHandler = (e) => {
-        setInputTea((prev) => ({
-          ...prev,
-          [e.target.name]: e.target.value,
-        }));
-      };
-    
-      const submitHandler = async (e) => {
-        e.preventDefault();
-        const response = await fetch('/tea/add', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify(inputTea),
-        });
-      };
-    return (
-        <>
-        <div>
-            <table className="table table-dark">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Имя</th>
-                <th scope="col">email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <th scope="row">Данные:</th>
-                <td>{userState.name}</td>
-                <td>{userState.email}</td>
-                </tr>
-            </tbody>
-            </table>
-        </div>
+      <div>
+        <h1>Добавить новый чай</h1>
+        <form onSubmit={submitHandler}>
+          <label htmlFor="username-input" className="block mar-b-1">Название чая:</label>
+          <input
+            id="username-input"
+            onChange={inputHandler}
+            value={inputTea.name}
+            name="name"
+            type="text"
+            className="block w-100 no-outline no-border pad-1 mar-b-2"
+          />
 
-    <div>
-      <h1>Добавить новый чай</h1>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="username-input" className="block mar-b-1">Название чая:</label>
-        <input
-          id="username-input"
-          onChange={inputHandler}
-          value={inputTea.name}
-          name="name"
-          type="text"
-          className="block w-100 no-outline no-border pad-1 mar-b-2"
-        />
+          <label htmlFor="email-input" className="block mar-b-1">Изображение:</label>
+          <input
+            id="email-input"
+            onChange={inputHandler}
+            value={inputTea.email}
+            name="img"
+            type="text"
+            className="block w-100 no-outline no-border pad-1 mar-b-2"
+          />
 
-        <label htmlFor="email-input" className="block mar-b-1">Изображение:</label>
-        <input
-          id="email-input"
-          onChange={inputHandler}
-          value={inputTea.email}
-          name="img"
-          type="text"
-          className="block w-100 no-outline no-border pad-1 mar-b-2"
-        />
+          <label htmlFor="password-input" className="block mar-b-1">Описание:</label>
+          <input
+            id="password-input"
+            onChange={inputHandler}
+            value={inputTea.password}
+            name="description"
+            type="text"
+            className="block w-100 no-outline no-border pad-1 mar-b-2"
+          />
 
-        <label htmlFor="password-input" className="block mar-b-1">Описание:</label>
-        <input
-          id="password-input"
-          onChange={inputHandler}
-          value={inputTea.password}
-          name="description"
-          type="text"
-          className="block w-100 no-outline no-border pad-1 mar-b-2"
-        />
+          <label htmlFor="password-input" className="block mar-b-1">Адрес:</label>
+          <input
+            id="password-input"
+            onChange={inputHandler}
+            value={inputTea.password}
+            name="location"
+            type="text"
+            className="block w-100 no-outline no-border pad-1 mar-b-2"
+          />
 
-        <label htmlFor="password-input" className="block mar-b-1">Адрес:</label>
-        <input
-          id="password-input"
-          onChange={inputHandler}
-          value={inputTea.password}
-          name="location"
-          type="text"
-          className="block w-100 no-outline no-border pad-1 mar-b-2"
-        />
+          <label htmlFor="password-input" className="block mar-b-1">По X:</label>
+          <input
+            id="password-input"
+            onChange={inputHandler}
+            value={inputTea.password}
+            name="x"
+            type="number"
+            className="block w-100 no-outline no-border pad-1 mar-b-2"
+          />
 
-        <label htmlFor="password-input" className="block mar-b-1">По X:</label>
-        <input
-          id="password-input"
-          onChange={inputHandler}
-          value={inputTea.password}
-          name="x"
-          type="number"
-          className="block w-100 no-outline no-border pad-1 mar-b-2"
-        />
+          <label htmlFor="password-input" className="block mar-b-1">По Y:</label>
+          <input
+            id="password-input"
+            onChange={inputHandler}
+            value={inputTea.password}
+            name="y"
+            type="number"
+            className="block w-100 no-outline no-border pad-1 mar-b-2"
+          />
 
-        <label htmlFor="password-input" className="block mar-b-1">По Y:</label>
-        <input
-          id="password-input"
-          onChange={inputHandler}
-          value={inputTea.password}
-          name="y"
-          type="number"
-          className="block w-100 no-outline no-border pad-1 mar-b-2"
-        />
-
-        <input
-          onChange={inputHandler}
-          type="submit"
-          value="Добавить"
-          className="block button w-100 mar-t-4 mar-b-3 pad-2 round-1 text-c center no-border no-outline"
-        />
-      </form>
-    </div>
-        <h2>Список всего чая</h2>
-        <div>
+          <input
+            onChange={inputHandler}
+            type="submit"
+            value="Добавить"
+            className="block button w-100 mar-t-4 mar-b-3 pad-2 round-1 text-c center no-border no-outline"
+          />
+        </form>
+      </div>
+      <h2>Список всего чая</h2>
+      <div>
         {allTea?.map((el) => (
-            <div key={el.id}>
-                <div className="card" style={{width: "18rem" }}>
-                <img src={el.img} className="card-img-top" alt={el.name}/>
-                <div className="card-body">
+          <div key={el.id}>
+            <div className="card" style={{ width: '18rem' }}>
+              <img src={el.img} className="card-img-top" alt={el.name} />
+              <div className="card-body">
                 <h5 className="card-title">{el.name}</h5>
                 <p classNmae="card-text">{el.description}</p>
                 <button type="button" className="btn btn-outline-success">редактировать</button>
                 <button type="button" className="btn btn-outline-danger">удалить</button>
-                </div>
-                </div>
+              </div>
             </div>
+          </div>
         ))}
-        </div>
-        </>
-    
-        
-    );
+      </div>
+    </>
+
+  );
 }
 
 export default AdminLk;
-
--- src/components/TeaDetails.jsx
+++ src/components/TeaDetails.jsx
@@ -22,11 +22,25 @@ function TeaDetails({ authState }) {
       .then((data) => {
         setTeaState(data);
       });
-    fetch(`/api/lk/${authState.id}`)
+    fetch(`/api/lk/${authState?.id}`)
       .then((res) => res.json())
-      .then((data) => setUserState(data));
-    setIsFavState(userState.favTeas.includes(teaState));
+      .then((data) => {
+        setUserState(data);
+        setIsFavState(data.favTeas.filter((el) => el.id === id).length > 0);
+      });
   }, []);
+
+  // console.log('>>>>>', userState.favTeas.includes(teaState.id));
+  // console.log('>>>>>', userState.favTeas.filter((el) => el.id === teaState.id));
+  // console.log('>>>>>', userState.favTeas.filter((el) => el.id === teaState.id)[0].id);
+
+  // useEffect(() => {
+  //   setIsFavState(userState.favTeas.filter((el) => el.id === teaState.id)[0].id === teaState.id);
+  // }, []);
+  // console.log('teaState -> ', teaState);
+  // console.log(isFavState);
+  // setIsFavState(userState.favTeas.includes(teaState));
+  // console.log(isFavState);
 
   const deleteHandler = async (e) => {
     e.preventDefault();
@@ -36,14 +50,13 @@ function TeaDetails({ authState }) {
       body: JSON.stringify({ userId: userState.user_id }),
     });
     if (response.ok) {
-      setTeaState({ name: 'Чай удален из избранного' });
       setIsFavState(!isFavState);
     }
   };
 
   const createHandler = async (e) => {
     e.preventDefault();
-    const response = await fetch(`api/fav/${id}`, {
+    const response = await fetch(`/api/fav/${id}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ userId: userState.user_id }),
@@ -57,35 +70,65 @@ function TeaDetails({ authState }) {
 
   return (
     <div className="row tea_detail">
-      <div className="col-lg-8 col-md-8 col-sm-12 desc" id="tea_detail_block">
-        <h3>
-          <font color="#3AC1EF">
-            ▍
-            {teaState?.name}
-          </font>
-        </h3>
-        <img align="center" src={teaState?.img} className="img-fluid" />
-        <h1>Описание: </h1>
-        <p>
-          {teaState?.description}
-        </p>
-        <h1>Локация: </h1>
-        <p>
-          {teaState?.location}
-        </p>
-      </div>
-      <div className="btns_container">
+      {isFavState === true
+        ? (
+          <>
+            <div className="col-lg-8 col-md-8 col-sm-12 desc" id="tea_detail_block">
+              <h3>
+                <font color="#3AC1EF">
+                  ▍
+                  {teaState?.name}
+                </font>
+              </h3>
+              <img align="center" src={teaState?.img} className="img-fluid" />
+              <h1>Описание: </h1>
+              <p>
+                {teaState?.description}
+              </p>
+              <h1>Локация: </h1>
+              <p>
+                {teaState?.location}
+              </p>
+            </div>
+            <div className="btns_container">
 
-        {userState.roleName === 'admin'
-          ? (
-            <>
+              {userState?.roleName === 'admin'
+                ? (
+                  <>
 
-              <button type="button" className="btn btn-dark">Редактировать</button>
-              <button type="button" className="btn btn-danger">Удалить</button>
-            </>
-          )
-          : isFavState === true ? <button type="button" onClick={createHandler} className="btn btn-secondary">Добавить в избранное</button> : <button onClick={deleteHandler} type="button" className="btn btn-danger">Удалить из избранного</button>}
-      </div>
+                    <button type="button" className="btn btn-dark">Редактировать</button>
+                    <button type="button" className="btn btn-danger">Удалить</button>
+                  </>
+                )
+                : isFavState === false ? <button type="button" onClick={createHandler} className="btn btn-secondary">Добавить в избранное</button> : <button onClick={deleteHandler} type="button" className="btn btn-danger">Удалить из избранного</button>}
+            </div>
+            {' '}
+
+          </>
+        ) : (
+          <>
+            <div className="col-lg-8 col-md-8 col-sm-12 desc" id="tea_detail_block">
+              <h3>
+                <font color="#3AC1EF">
+                  ▍
+                  Чай удален из избранного
+                </font>
+              </h3>
+            </div>
+            <div className="btns_container">
+
+              {userState?.roleName === 'admin'
+                ? (
+                  <>
+
+                    <button type="button" className="btn btn-dark">Редактировать</button>
+                    <button type="button" className="btn btn-danger">Удалить</button>
+                  </>
+                )
+                : isFavState === false ? <button type="button" onClick={createHandler} className="btn btn-secondary">Добавить в избранное</button> : <button onClick={deleteHandler} type="button" className="btn btn-danger">Удалить из избранного</button>}
+            </div>
+          </>
+        ) }
 
     </div>
   );
